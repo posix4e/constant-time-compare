b872c3c54899c9e30ad0d57e7f104d4b0f8e249d
Alex Newman <alex.newman@wandisco.com>
2015-05-20 09:25:29 -0700

    Switch to a more scalable differencing algorithm
    	Fix some spacing with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,54 @@
 
 use std::vec::Vec;
-use std::i64::MAX;
 
-fn is_equal(a: Vec<u8>, b:Vec<u8>) -> bool {
-    number_of_different_chars(a, b) == 0
+fn is_equal(a: Vec<u8>, b: Vec<u8>) -> bool {
+    number_mash(a, b) == 0
 }
 
 #[inline(never)] // This should be inlined to prevent another function from optimizing it to not
                  // be constant time.
-fn number_of_different_chars(a: Vec<u8>, b :Vec<u8>) -> i32 {
+fn number_mash(a: Vec<u8>, b: Vec<u8>) -> u8 {
     if a.len() != b.len() {
-        -1
-    } else if (a.len() as i64) > MAX {
-        panic!("Unable to compare such large arrays of bytes. Please keep the size under a billion or so");
+        panic!("Number Mash can only compare equal sized arrays");
     } else {
-        let mut result: i32 = 0;
+        let mut result = 0;
         for (&x, &y) in a.iter().zip(b.iter()) {
-            let diff = x ^ y;
-            result = result + ( diff as i32);
+            result |= x ^ y;
         }
         result
     }
 }
 
 #[test]
-fn test_number_of_different_chars() {
-    assert!(0 == number_of_different_chars(vec![1,2,3,4], vec![1,2,3,4]));
-    assert!(1 == number_of_different_chars(vec![1,2,3,4], vec![1,2,3,5]));
-    assert!( 0 == number_of_different_chars(vec![1], vec![1]));
-    assert!(-1 == number_of_different_chars(vec![], vec![1]));
+fn test_number_mash_works_as_expected() {
+    assert!(0 == number_mash(vec![1,2,3,4], vec![1,2,3,4]));
+    assert!(1 == number_mash(vec![1,2,3,4], vec![1,2,3,5]));
+    assert!(250 == number_mash(vec![1,2,3,4], vec![1,2,3,254]));
+    assert!( 0 == number_mash(vec![1], vec![1]));
+}
+
+#[test]
+#[should_panic]
+fn number_mash_will_panic_for_different_sized_arrays() {
+    number_mash(vec![1,2,3], vec![1,2,3,5]);
 }
 
 
 #[test]
 fn test_is_equal() {
     assert!(is_equal(vec![1,2,3,4], vec![1,2,3,4]));
-    assert!(!is_equal(vec![1,2,3,4], vec![1,2,3,]));
     assert!(is_equal(vec![1], vec![1]));
-    assert!(!is_equal(vec![], vec![1]));
+
+}
+
+#[test]
+#[should_panic]
+fn eq_will_panic_for_different_sized_arrays() {
+    is_equal(vec![1,2,3,4], vec![1,2,3,]);
+}
+
+#[test]
+#[should_panic]
+fn eq_with_null_veq_will_panic() {
+    is_equal(vec![], vec![1]);
 }
